670aeb6ae18c11261060ca23518857756e35145a
Manh Cuong <cuongnm4215@gmail.com>
2016-11-16 11:20:38 +0700

    add Reading data and logic

--- app/components/reading/reading.component.ts
+++ app/components/reading/reading.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 import { ReadingService } from './reading.service';
 
@@ -9,8 +10,14 @@ import { ReadingService } from './reading.service';
 })
 export class ReadingComponent implements OnInit{ 
 	questions: Object[];
+  param: number;
+  lower_limit: number;
+  upper_limit: number;
+
 	countCorrectAnswer: number;
-	constructor(private readingService:ReadingService) {
+	constructor(private readingService:ReadingService,
+      private router: Router
+    ) {
 		this.readingService.getQuestions()
 			.subscribe(questions => {
 				this.questions = questions;
@@ -18,24 +25,32 @@ export class ReadingComponent implements OnInit{
   	}
 
   	ngOnInit() {
-  		
+  		this.param = 1;
+      this.upper_limit = this.param * 10;
+      this.lower_limit = this.upper_limit - 9;
   	}
 
+    gotoLesson(param: number): void{
+      this.router.navigate(['/reading', param]);
+      this.param = param;
+      this.upper_limit = this.param * 10;
+      this.lower_limit = this.upper_limit - 9;
+    }
 
-  saveStatus(question: Object, value:String) {
-    question['option'] = value;
-  }
+    saveStatus(question: Object, value:String) {
+      question['option'] = value;
+    }
 
-  check() {
-    this.countCorrectAnswer = 0;
+    check() {
+      this.countCorrectAnswer = 0;
 
-    for (let i = 0; i < this.questions.length; i ++){
-      if(this.questions[i]['option'] == this.questions[i]['correct_answer']){
-        this.countCorrectAnswer++;
-        this.questions[i]['status'] = "Right";
-      }else{
-        this.questions[i]['status'] = "Wrong! The correct answer is " + this.questions[i]['correct_answer'];
+      for (let i = 0; i < this.questions.length; i ++){
+        if(this.questions[i]['option'] == this.questions[i]['correct_answer']){
+          this.countCorrectAnswer++;
+          this.questions[i]['status'] = "Right";
+        }else{
+          this.questions[i]['status'] = "Wrong! The correct answer is " + this.questions[i]['correct_answer'];
+        }
       }
     }
-  }
 }
